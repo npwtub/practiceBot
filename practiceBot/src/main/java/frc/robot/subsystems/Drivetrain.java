4a9420ac9dae1821e16ce8b1708ee016cbd1a8da
Lu-han-wang <luhanwang5@gmail.com>
2023-11-13 17:26:35 -0500

    Updated drivetrain for future PID practice

--- practiceBot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ practiceBot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,10 +1,17 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.sensors.Pigeon2;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class Drivetrain extends SubsystemBase{
     private CANSparkMax leftFront;
@@ -16,6 +23,13 @@ public class Drivetrain extends SubsystemBase{
     private double rotation;
     private boolean quickTurn;
 
+    private RelativeEncoder leftFrontEncoder;
+	private RelativeEncoder leftRearEncoder;
+	private RelativeEncoder rightFrontEncoder;
+	private RelativeEncoder rightRearEncoder;
+
+    private Pigeon2 pigeon = new Pigeon2(0);
+    private DifferentialDriveOdometry odometry;
 
     public Drivetrain(){
         initializeMotor(leftFront);
@@ -25,7 +39,13 @@ public class Drivetrain extends SubsystemBase{
 
         setFollowers();
 
+        leftFrontEncoder = this.leftFront.getEncoder();
+		rightFrontEncoder = this.rightFront.getEncoder();
+		leftRearEncoder = this.leftRear.getEncoder();
+		rightRearEncoder = this.rightRear.getEncoder();
+
         diffDrive  = new DifferentialDrive(leftFront, rightFront);
+        odometry = new DifferentialDriveOdometry(this.getRotation2d(), this.leftFrontEncoder.getPosition(), this.rightFrontEncoder.getPosition());
     }
 
     private void initializeMotor(CANSparkMax motor){
@@ -45,6 +65,14 @@ public class Drivetrain extends SubsystemBase{
         this.quickTurn = quickTurn;
     }
 
+    public Rotation2d getRotation2d(){
+        double degrees = -MathUtil.inputModulus(this.pigeon.getYaw(), -180, 180);
+        return Rotation2d.fromDegrees(degrees);
+    }
+
+    public Pose2d getPose2d(){
+        return this.odometry.getPoseMeters();
+    }
     @Override
     public void periodic() {
         diffDrive.curvatureDrive(this.speed, this.rotation, this.quickTurn);
